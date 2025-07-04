5cf5d1bf8614f0f3e1ab335c409be9a88759d9a8
HenryC0416 <henrychen0416@gmail.com>
2025-07-04 16:52:32 -0400

    update

---
+++ Trivia/questions.swift
@@ -0,0 +1,23 @@
+//
+//  questions.swift
+//  Trivia
+//
+//  Created by Henry Chen on 6/19/25.
+//
+
+import Foundation
+import UIKit
+
+
+
+struct Questions{
+    let questionNum: String
+    let topic: String
+    let question: String
+    let choices: [String]
+    let answer: Int
+}
+
+
+
+
